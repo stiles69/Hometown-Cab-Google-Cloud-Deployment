66bb5afe191a61544775d1b0fd30aa4d2bfd85e0
stiles69 <brett.salemink@gmail.com>
2018-05-13 07:41:40 -0500

    Initial Commit

---
+++ Webserver1/app.go
@@ -0,0 +1,15 @@
+package hometownmaterialdesign
+
+import (
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", static)
+}
+
+func static(w http.ResponseWriter, r *http.Request) {	
+	http.ServeFile(w, r, "files/"+r.URL.Path)
+}
+
+
---
+++ Webserver1/files/precache-manifest.0acd85bb58277878bc56dada74500d0f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "d0439cd7bb0f1122184e",
+    "url": "/js/vendor.d0439cd7.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "8e42a075fde5b1ea4f33",
+    "url": "/js/app.8e42a075.js"
+  },
+  {
+    "revision": "8272df885578fcbaa6f2d6166db6a23b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73c49f53fb21c7f1268801122db2ceac",
+    "url": "/img/Logo-192x192.svg"
+  },
+  {
+    "revision": "3d7e17211ada3c4030c43939141451d3",
+    "url": "/img/Cover-1920x950.3d7e1721.png"
+  },
+  {
+    "revision": "357765ab35f4d413eeccaf44276f68e0",
+    "url": "/img/404Error.svg"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "8e42a075fde5b1ea4f33",
+    "url": "/css/app.2fa21716.css"
+  }
+];
